a60c279a5d9d65ef17d76ecfee68456fe6ad3831
dawgaw <unconfigured@null.spigotmc.org>
2022-08-21 00:52:22 +0300

    optimize

--- Stream.Profiling/Generator.cs
+++ Stream.Profiling/Generator.cs
@@ -4,39 +4,41 @@ using System.Linq;
 
 namespace Stream.Profiling
 {
-    internal sealed class Generator
-    {
-        private readonly Random random = new Random();
-        private readonly string[] words;
+	internal sealed class Generator
+	{
+		private readonly Random random = new Random();
+		private readonly string[] words;
 
-        public Generator()
-        {
-            words = Enumerable.Range(0, 10000)
-                .Select(
-                    x =>
-                    {
-                        var range = Enumerable.Range(0, random.Next(20, 100));
-                        var chars = range.Select(x => (char)random.Next('A', 'Z')).ToArray();
-                        var str = new string(chars);
-                        return str;
-                    }).ToArray();
-        }
+		public Generator()
+		{
+			words = Enumerable.Range(0, 10000)
+				.Select(
+					x =>
+					{
+						var range = Enumerable.Range(0, random.Next(20, 100));
+						var chars = range.Select(x => (char)random.Next('A', 'Z')).ToArray();
+						var str = new string(chars);
+						return str;
+					}).ToArray();
+		}
 
-        public string Generate(int linesCount)
-        {
-            var fileName = "L" + linesCount + ".txt";
-            using (var writer = new StreamWriter(fileName))
-            {
-                for (int i = 0; i < linesCount; i++)
-                {
-                    writer.WriteLine(GenerateNumber() + ". " + GenerateString());
-                }
-            }
-            return fileName;
-        }
+		public string Generate(int linesCount)
+		{
+			var fileName = "L" + linesCount + ".txt";
+			if (File.Exists(fileName))
+				return fileName;
+			using (var writer = new StreamWriter(fileName))
+			{
+				for (int i = 0; i < linesCount; i++)
+				{
+					writer.WriteLine(GenerateNumber() + ". " + GenerateString());
+				}
+			}
+			return fileName;
+		}
 
-        private string GenerateString() => words[random.Next(0, words.Length)];
+		private string GenerateString() => words[random.Next(0, words.Length)];
 
-        private string GenerateNumber() => random.Next(0, 10000).ToString();
-    }
+		private string GenerateNumber() => random.Next(0, 10000).ToString();
+	}
 }
--- Stream.Profiling/Program.cs
+++ Stream.Profiling/Program.cs
@@ -4,185 +4,197 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Stream.Profiling
 {
-    struct Line : IComparable<Line>
-    {
-        private int pos;
-        private string line;
-        public Line(string line)
-        {
-            pos = line.IndexOf(".");
-            Number = int.Parse(line.AsSpan(0, pos));
-            this.line = line;
-        }
+	struct Line : IComparable<Line>
+	{
+		private int pos;
+		private string line;
+		public Line(string line)
+		{
+			pos = line.IndexOf(".");
+			Number = int.Parse(line.AsSpan(0, pos));
+			this.line = line;
+		}
 
-        public string Build() => line;
+		public string Build() => line;
 
-        public int Number { get; set; }
-        public ReadOnlySpan<char> Word => line.AsSpan(pos + 2);
+		public int Number { get; set; }
+		public ReadOnlySpan<char> Word => line.AsSpan(pos + 2);
 
-        public int CompareTo(Line other)
-        {
-            int result = Word.CompareTo(other.Word, StringComparison.Ordinal);
-            if (result != 0)
-                return result;
-            return Number.CompareTo(other.Number);
-        }
-    }
+		public int CompareTo(Line other)
+		{
+			int result = Word.CompareTo(other.Word, StringComparison.Ordinal);
+			if (result != 0)
+				return result;
+			return Number.CompareTo(other.Number);
+		}
+	}
 
 
-    class Sorter
-    {
-        public async Task Sort(string fileName, int partLinesCount)
-        {
-            var files = await SplitFile(fileName, partLinesCount);
-            //SortParts(files, partLinesCount);
-            await SortResult(files);
-        }
+	class Sorter
+	{
+		public void Sort(string fileName, int partLinesCount)
+		{
+			var files = SplitFile(fileName, partLinesCount);
+			//SortParts(files, partLinesCount);
+			SortResult(files);
+		}
 
-        private void SortParts(string[] files, int partLinesCount)
-        {
-            Line[] lines = new Line[partLinesCount];
-            foreach(var file in files)
-            {
-                var strings = File.ReadAllLines(file);
-                for (int i = 0; i < strings.Length; i++)
-                {
-                    lines[i] = new Line(strings[i]);
-                }
+		private void SortParts(string[] files, int partLinesCount)
+		{
+			Line[] lines = new Line[partLinesCount];
+			foreach (var file in files)
+			{
+				var strings = File.ReadAllLines(file);
+				for (int i = 0; i < strings.Length; i++)
+				{
+					lines[i] = new Line(strings[i]);
+				}
 
-                Array.Sort(lines, 0, strings.Length);
-                File.WriteAllLines(file, lines.Select(x => x.Build()));
-            }
-        }
+				Array.Sort(lines, 0, strings.Length);
+				File.WriteAllLines(file, lines.Select(x => x.Build()));
+			}
+		}
 
-        private class LineState
-        {
-            public StreamReader Reader { get; set; }
-            public Line Line { get; set; }
-        }
+		private class LineState
+		{
+			public StreamReader Reader { get; set; }
+			public Line Line { get; set; }
+		}
 
-        private async Task SortResult(string[] files)
-        {
-            var readers = files.Select(x => new StreamReader(x));
-            try
-            {
-                var lines = readers.Select(x => new LineState
-                {
-                    Line = new Line(x.ReadLine()),
-                    Reader = x
-                }).OrderBy(x => x.Line).ToList();
+		private void SortResult(string[] files)
+		{
+			var readers = files.Select(x => new StreamReader(x, System.Text.Encoding.Default, true, 1024 * 1024 * 2));
+			try
+			{
+				var lines = readers.Select(x => new LineState
+				{
+					Line = new Line(x.ReadLine()),
+					Reader = x
+				}).OrderBy(x => x.Line).ToList();
 
-                using var writer = new StreamWriter("result.txt");
-                while (lines.Count > 0)
-                {
-                    var current = lines[0];
-                    writer.WriteLine(current.Line.Build());
 
-                    if (current.Reader.EndOfStream)
-                    {
-                        lines.Remove(current);
-                        continue;
-                    }
-                    current.Line = new Line(await current.Reader.ReadLineAsync());
-                    Reorder(lines);
-                }
-            }
-            finally
-            {
-                foreach (var r in readers)
-                    r.Dispose();
-            }
-        }
+				using var writer = new StreamWriter("result.txt", false, System.Text.Encoding.Default, 1024 * 1024 * 20);
+				while (lines.Count > 0)
+				{
+					var current = lines[0];
+					writer.WriteLine(current.Line.Build());
 
-        private void Reorder(List<LineState> lines)
-        {
-            if (lines.Count == 1)
-                return;
+					if (current.Reader.EndOfStream)
+					{
+						lines.Remove(current);
+						continue;
+					}
+					current.Line = new Line(current.Reader.ReadLine());
+					Reorder(lines);
+				}
+			}
+			finally
+			{
+				foreach (var r in readers)
+					r.Dispose();
+			}
+		}
 
-            int i = 0;
-            while (lines[i].Line.CompareTo(lines[i + 1].Line) > 0)
-            {
-                var t = lines[i];
-                lines[i] = lines[i + 1];
-                lines[i + 1] = t;
-                i++;
-                if (i + 1 == lines.Count)
-                    return;
-            }
-        }
+		private void Reorder(List<LineState> lines)
+		{
+			if (lines.Count == 1)
+				return;
 
-        private IEnumerable<Line[]> Batch(IEnumerable<string> lines, int partLinesCount)
-        {
-            Line[] l = new Line[partLinesCount];
-            int i = 0;
-            foreach(var line in lines)
-            {
-                l[i] = new Line(line);
-                i++;
-                if (i == partLinesCount)
-                {
-                    yield return l;
-                    i = 0;
-                }
-            }
+			int i = 0;
+			while (lines[i].Line.CompareTo(lines[i + 1].Line) > 0)
+			{
+				var t = lines[i];
+				lines[i] = lines[i + 1];
+				lines[i + 1] = t;
+				i++;
+				if (i + 1 == lines.Count)
+					return;
+			}
+		}
 
-            if (i != 0)
-            {
-                Array.Resize(ref l, i);
-                yield return l;
-            }
-        }
+		private IEnumerable<Line[]> Batch(IEnumerable<string> lines, int partLinesCount)
+		{
+			Line[] l = new Line[partLinesCount];
+			int i = 0;
+			foreach (var line in lines)
+			{
+				l[i] = new Line(line);
+				i++;
+				if (i == partLinesCount)
+				{
+					yield return l;
+					i = 0;
+				}
+			}
 
-        private async Task<string[]> SplitFile(string fileName, int partLinesCount)
-        {
-            var list = new List<string>();
-            int partNumber = 0;
-            Line[] lines = new Line[partLinesCount];
-            int i = 0;
+			if (i != 0)
+			{
+				Array.Resize(ref l, i);
+				yield return l;
+			}
+		}
 
-            using var reader = new StreamReader(fileName);
-            for (string line = await reader.ReadLineAsync();
-                !reader.EndOfStream; 
-                line = await reader.ReadLineAsync())
-            {
-                lines[i] = new Line(line);
-                i++;
-                if (i == partLinesCount)
-                {
-                    partNumber++;
-                    var partFileName = partNumber + ".txt";
-                    list.Add(partFileName);
-                    Array.Sort(lines);
-                    await File.WriteAllLinesAsync(partFileName, lines.Select(x => x.Build()));
-                    i = 0;
-                }
-            }
+		private string[] SplitFile(string fileName, int partLinesCount)
+		{
+			List<Task> tasks = new List<Task>();
 
-            if (i == 0)
-            {
-                Array.Resize(ref lines, i);
-                var partFileName = partNumber + ".txt";
-                list.Add(partFileName);
-                await File.WriteAllLinesAsync(partFileName, lines.Select(x => x.Build()));
-            }
+			var list = new List<string>();
+			int partNumber = 0;
+			Line[] lines = new Line[partLinesCount];
+			int i = 0;
 
-            return list.ToArray();
-        }
-    }
+			using var reader = new StreamReader(fileName);
+			for (string line = reader.ReadLine();
+				!reader.EndOfStream;
+				line = reader.ReadLine())
+			{
+				lines[i] = new Line(line);
+				i++;
+				if (i == partLinesCount)
+				{
+					partNumber++;
+					var partFileName = partNumber + ".txt";
+					list.Add(partFileName);
+					Array.Sort(lines);
+					tasks.Add(File.WriteAllLinesAsync(partFileName, lines.Select(x => x.Build())));
+					i = 0;
+				}
+			}
 
-    class Program
-    {
-        static async Task Main(string[] args)
-        {
-            var sw = Stopwatch.StartNew();
-            var fileName = new Generator().Generate(2_000_000);
-            await new Sorter().Sort(fileName, 200_000);
-            sw.Stop();
-            Console.WriteLine($"Execution took: {sw.Elapsed}");
-        }
-    }
+			if (i != 0)
+			{
+				Array.Resize(ref lines, ++i);
+				var partFileName = ++partNumber + ".txt";
+				list.Add(partFileName);
+				Array.Sort(lines);
+				File.WriteAllLines(partFileName, lines.Select(x => x.Build()));
+			}
+			Task.WaitAll(tasks.ToArray());
+			return list.ToArray();
+		}
+	}
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var fileName = new Generator().Generate(2_000_000);
+			var sw = Stopwatch.StartNew();
+			new Sorter().Sort(fileName, 200_000);
+			sw.Stop();
+			Console.WriteLine($"Execution took: {sw.Elapsed}");
+			Regex tempFiles = new Regex(@"^\d+\.txt");
+
+			new DirectoryInfo(Directory.GetCurrentDirectory()).GetFiles("*.txt").ForEach(x =>
+			{
+				if (tempFiles.IsMatch(x.Name))
+					x.Delete();
+			});
+		}
+	}
 }
